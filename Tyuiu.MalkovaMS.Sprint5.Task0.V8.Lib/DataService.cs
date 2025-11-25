fe9fbecf375021f9abf55e9b7eb02bb3a8be055b
Мария Малькова <Tyuiu.MalkovaMS@outlook.com>
2025-11-25 20:48:22 +0500

    Task0

--- Tyuiu.MalkovaMS.Sprint5.Task0.V8.Lib/DataService.cs
+++ Tyuiu.MalkovaMS.Sprint5.Task0.V8.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.MalkovaMS.Sprint5.Task0.V8.Lib
         public string SaveToFileTextData(int x)
         {
             string path = Path.Combine(Path.GetTempPath(),"OutPutFileTask0.txt");
-            double y = Math.Round((Math.Pow(x, 3) - 1) / 4 * x * x, 3);
+            double y = Math.Round((Math.Pow(x, 3) - 1) / (4 * x * x), 3);
             File.WriteAllText(path, Convert.ToString(y));
             return path;
         }
