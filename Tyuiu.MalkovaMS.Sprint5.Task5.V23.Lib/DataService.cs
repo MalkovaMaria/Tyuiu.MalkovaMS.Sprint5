bd3cc82f2c8beed628dd73b6a6d8b7e5b8b2cf02
Мария Малькова <Tyuiu.MalkovaMS@outlook.com>
2025-11-26 23:43:09 +0500

    Task5

--- Tyuiu.MalkovaMS.Sprint5.Task5.V23.Lib/DataService.cs
+++ Tyuiu.MalkovaMS.Sprint5.Task5.V23.Lib/DataService.cs
@@ -1,7 +1,24 @@
-﻿namespace Tyuiu.MalkovaMS.Sprint5.Task5.V23.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
 
+namespace Tyuiu.MalkovaMS.Sprint5.Task5.V23.Lib
+{
+    public class DataService : ISprint5Task5V23
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string str = File.ReadAllText(path).Replace(".", ",");
+            string[] nums = str.Split(' ');
+            double[] nums1 = new double[nums.Length];
+            double res = 100;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums1[i] = double.Parse(nums[i]);
+                if (nums1[i] % 1 != 0 & nums1[i] < res)
+                    res = nums1[i];
+            }
+            return res;
+            
+        }
     }
 }
--- Tyuiu.MalkovaMS.Sprint5.Task5.V23/Program.cs
+++ Tyuiu.MalkovaMS.Sprint5.Task5.V23/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.MalkovaMS.Sprint5.Task5.V23.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        string path = @"C:\DataSprint5\InPutDataFileTask5V23.txt";
+        Console.Title = "Спринт #5 | Выполнила: Малькова М. С. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Чтение набора данных из текстового файла                                *");
+
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #23                                                             *");
+        Console.WriteLine("* Выполнила: Малькова Мария Сергеевна | ИИПб-25-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Найти минимальное вещественное число в файле.                           *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine($"Данные находятся в файле {path}");
+        Console.WriteLine("***************************************************************************");
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Минимальное вещественное число: " + res);
+        Console.ReadKey();
+    }
+}
